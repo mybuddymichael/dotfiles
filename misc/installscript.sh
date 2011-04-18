f3315d1f081ea354a8f448b571c6a16490c2b428
Michael Hanson <michael.b.hanson@gmail.com>
2011-04-18 14:20:17 -0700

    Very rough first commit of an installscript

---
+++ misc/installscript.sh
@@ -0,0 +1,4 @@
+			https://dl-ssl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+			hdiutil attach googlechrome.dmg 
+			cd /Volumes/Google\ Chrome/
+			cp -iv Google\ Chrome.app ~/Applications/Google\ Chrome.app
