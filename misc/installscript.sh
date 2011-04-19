95f7f2530235e3e672dd2c5dff053b8529845ef0
Michael Hanson <michael.b.hanson@gmail.com>
2011-04-18 18:15:51 -0700

    Make Google Chrome install work

--- misc/installscript.sh
+++ misc/installscript.sh
@@ -1,4 +1,13 @@
-			https://dl-ssl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
-			hdiutil attach googlechrome.dmg 
-			cd /Volumes/Google\ Chrome/
-			cp -iv Google\ Chrome.app ~/Applications/Google\ Chrome.app
+	[[ -d ~/installtmp ]] && mkdir ~/installtmp
+	INSTALL_ROOT = ~/installtmp
+	cd $INSTALL_ROOT
+
+
+# Install Google Chrome.
+
+	echo '*** Download Google Chrome...'
+	curl -# -O https://dl-ssl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+	hdiutil attach -quiet -noautoopen $INSTALL_ROOT/googlechrome.dmg
+	cp -iv /Volumes/Google\ Chrome/Google\ Chrome.app ~/Applications/Google\ Chrome.app
+	hdiutil detach -quiet /Volumes/Google\ Chrome/
+	rm -f $INSTALL_ROOT/googlechrome.dmg
