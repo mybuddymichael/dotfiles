01894e46e6f5129ee34ca2f527921868ad9d27cc
Michael Hanson <michael.b.hanson@gmail.com>
2011-04-18 21:33:58 -0700

    Add homebrew section
    :

--- misc/installscript.sh
+++ misc/installscript.sh
@@ -2,10 +2,17 @@
 	INSTALL_ROOT = ~/installtmp
 	cd $INSTALL_ROOT
 
+# Install Homebrew.
+
+	echo '*** Installing Homebrew...'
+	rm -rf /usr/local/include
+	rm -rf /usr/local/lib
+	ruby -e "$(curl -fsSLk https://gist.github.com/raw/323731/install_homebrew.rb)"
+
 
 # Install Google Chrome.
 
-	echo '*** Download Google Chrome...'
+	echo '*** Downloading and installing Google Chrome...'
 	curl -OLkf# https://dl-ssl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
 	hdiutil attach -quiet -noautoopen $INSTALL_ROOT/googlechrome.dmg
 	cp -iv /Volumes/Google\ Chrome/Google\ Chrome.app ~/Applications/Google\ Chrome.app
