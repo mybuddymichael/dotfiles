266a1959a373790174a42dc4a596c43260a4e206
Michael Hanson <michael.b.hanson@gmail.com>
2011-04-18 22:00:22 -0700

    Formatting

--- misc/installscript.sh
+++ misc/installscript.sh
@@ -25,5 +25,5 @@
 	curl -OLkf# https://dl-ssl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
 	hdiutil attach -quiet -noautoopen $INSTALL_ROOT/googlechrome.dmg
 	cp -iv /Volumes/Google\ Chrome/Google\ Chrome.app ~/Applications/Google\ Chrome.app
-	hdiutil detach -quiet /Volumes/Google\ Chrome/
+	hdiutil detach -quiet /Volumes/Google\ Chrome
 	rm -f $INSTALL_ROOT/googlechrome.dmg
