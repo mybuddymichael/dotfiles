52cc758cb5a7f2f0079f73d7c6a2b0a4fb3d3f97
Michael Hanson <michael.b.hanson@gmail.com>
2011-04-19 13:12:30 -0700

    Add script to link files from workenv to /Users/michael

---
+++ misc/workenvsetup.sh
@@ -0,0 +1,24 @@
+# Set it up.
+
+	ORIGINAL_DIR = `pwd`
+	BASE_DIR = workenv/
+	cd ~
+
+
+# Link and copy files.
+
+	ln -nfs $BASE_DIR/bin/ bin
+
+	ln -nfs $BASE_DIR/bash/gitignore_global .gitignore_global
+	ln -nfs $BASE_DIR/bash/profile .profile
+	ln -nfs $BASE_DIR/bash/bashrc .bashrc
+	cp -iv  $BASE_DIR/bash/gitconfig .gitconfig
+
+	ln -nfs $BASE_DIR/vim/ vim
+	ln -nfs $BASE_DIR/vim/vimrc .vimrc
+	ln -nfs $BASE_DIR/vim/gvimrc .gvimrc
+
+
+# Change back to the original directory.
+
+	cd $ORIGINAL_DIR
