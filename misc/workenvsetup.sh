9dce359114e5b24d0062ee6b3c7b5759d4cefca0
Michael Hanson <michael.b.hanson@gmail.com>
2011-04-20 12:01:22 -0700

    Add -v flag to ln

--- misc/workenvsetup.sh
+++ misc/workenvsetup.sh
@@ -7,16 +7,16 @@
 
 # Link and copy files.
 
-	ln -nfs $BASE_DIR/bin/ bin
+	ln -nfsv $BASE_DIR/bin/ bin
 
-	ln -nfs $BASE_DIR/bash/bashrc .bashrc
-	ln -nfs $BASE_DIR/bash/profile .profile
-	ln -nfs $BASE_DIR/bash/gitignore_global .gitignore_global
-	cp -iv  $BASE_DIR/bash/gitconfig .gitconfig
+	ln -nfsv $BASE_DIR/bash/bashrc .bashrc
+	ln -nfsv $BASE_DIR/bash/profile .profile
+	ln -nfsv $BASE_DIR/bash/gitignore_global .gitignore_global
+	cp -iv   $BASE_DIR/bash/gitconfig .gitconfig
 
-	ln -nfs $BASE_DIR/vim/ .vim
-	ln -nfs $BASE_DIR/vim/vimrc .vimrc
-	ln -nfs $BASE_DIR/vim/gvimrc .gvimrc
+	ln -nfsv $BASE_DIR/vim/ .vim
+	ln -nfsv $BASE_DIR/vim/vimrc .vimrc
+	ln -nfsv $BASE_DIR/vim/gvimrc .gvimrc
 
 
 # Change back to the original directory.
