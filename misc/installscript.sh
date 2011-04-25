974bfd130811b1d2f7ba22073e94377c65319e11
Michael Hanson <michael.b.hanson@gmail.com>
2011-04-25 15:44:59 -0700

    Add several tests to install log, and formatting

--- misc/installscript.sh
+++ misc/installscript.sh
@@ -14,9 +14,11 @@
 # Install Homebrew.
 
 	echo '*** Installing Homebrew...'
+
 	sudo rm -rf /usr/local/include
 	sudo rm -rf /usr/local/lib
 	ruby -e "$(curl -fsSLk https://gist.github.com/raw/323731/install_homebrew.rb)"
+
 	if [[ `which brew` ]]; then
 		echo 'Homebrew installed' >> $INSTALL_LOG
 	else
@@ -27,7 +29,9 @@
 # Install Git.
 
 	echo '*** Installing Git...'
+
 	brew install git
+
 	if [[ `which git` ]]; then
 		echo 'Git installed' >> $INSTALL_LOG
 	else
@@ -38,28 +42,40 @@
 # Install my work environment.
 
 	echo '*** Installing workenv...'
+
 	cd ~
 	git pull git@github.com:thePapacy/workenv.git
 	. ~/workenv/misc/workenvsetup.sh
+
+	if [[ -d ~/workenv ]]; then
+		echo 'workenv/ installed' >> $INSTALL_LOG
+	else
+		echo 'WORKENV/ NOT INSTALLED' >> $INSTALL_LOG
+	fi
+
 	cd $INSTALL_ROOT
 
 
 # Install bash-completion.
 
 	echo '*** Installing bash-completion...'
+
 	brew install bash-completion
 
 
 # Install hub.
 
 	echo '*** Installing hub...'
+
 	brew install hub
 
 
 # Install RVM.
 
 	echo '*** Installing Ruby Version Manager...'
+
 	bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+
 	if [[ `which rvm` ]]; then
 		echo 'RVM installed' >> $INSTALL_LOG
 	else
@@ -70,20 +86,29 @@
 # Install MacVim.
 
 	echo '*** Installing MacVim...'
+
 	curl -OLkf# https://github.com/downloads/b4winckler/macvim/MacVim-snapshot-57.tbz
 	tar -xf MacVim-snapshot-57.tbz && mv MacVim-snapshot-57 MacVim
 	cp MacVim/MacVim.app /Applications/MacVim.app
 	rm -rf MacVim MacVim-snapshot-57.tbz
 
+	if [[ -s /Applications/MacVim.app ]]; then
+		echo 'MacVim installed' >> $INSTALL_LOG
+	else
+		echo 'MACVIM NOT INSTALLED' >> $INSTALL_LOG
+	fi
+
 
 # Install Google Chrome.
 
 	echo '*** Downloading and installing Google Chrome...'
+
 	curl -OLkf# https://dl-ssl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
 	hdiutil attach -quiet -noautoopen $INSTALL_ROOT/googlechrome.dmg
 	cp -iv /Volumes/Google\ Chrome/Google\ Chrome.app ~/Applications/Google\ Chrome.app
 	hdiutil detach -quiet /Volumes/Google\ Chrome
 	rm -f $INSTALL_ROOT/googlechrome.dmg
+
 	if [[ -s ~/Applications/Google\ Chrome.app ]]; then
 		echo 'Google Chrome installed' >> $INSTALL_LOG
 	else
@@ -94,11 +119,13 @@
 # Install Rdio client.
 
 	echo '*** Installing Rdio...'
+
 	curl -OLkf# http://www.rdio.com/media/desktop/mac/Rdio.dmg
 	hdiutil attach -quiet -noautoopen $INSTALL_ROOT/Rdio.dmg
 	cp -iv /Volumes/Rdio/Rdio.app ~/Applications/Rdio.app
 	hdiutil detach -quiet /Volumes/Rdio
 	rm -f $INSTALL_ROOT/Rdio.dmg
+
 	if [[ -s ~/Applications/Rdio.app ]]; then
 		echo 'Rdio installed' >> $INSTALL_LOG
 	else
@@ -109,7 +136,14 @@
 # Install 1password.
 
 	echo '*** Installing 1password'
+
 	curl -OLkf# http://aws.cachefly.net/aws/dmg/1PW3/English/1Password-3.5.9.zip
 	unzip 1Password-3.5.9.zip
 	mv -iv $INSTALL_ROOT/1Password.app ~/Applications/1Password.app
 	rm 1Password-3.5.9.zip
+
+	if [[ -s ~/Applications/1password.app ]]; then
+		echo '1password installed' >> $INSTALL_LOG
+	else
+		echo '1PASSWORD NOT INSTALLED' >> $INSTALL_LOG
+	fi
