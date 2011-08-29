cde6acd7aa6b263f7ebc0fb52abf572fb67b0185
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-29 16:46:40 -0700

    Pass stderr to /dev/null in rbenv_prompt_info function

--- zsh/rbenv.zsh
+++ zsh/rbenv.zsh
@@ -1,4 +1,4 @@
 function rbenv_prompt_info() {
-  version_string=$(rbenv version) || return
+  version_string=$(rbenv version 2> /dev/null) || return
   echo "`expr "$version_string" : '\([^ ]*\)'` "
 }
