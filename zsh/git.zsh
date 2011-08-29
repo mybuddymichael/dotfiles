472b75952f25ae9ff8a4ce5e0ee91df8a1abd8be
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-29 14:32:27 -0700

    Remove unneeded git.zsh functions

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -13,23 +13,6 @@ parse_git_dirty() {
   fi
 }
 
-# Checks if there are commits ahead from remote
-function git_prompt_ahead() {
-  if $(echo "$(git log origin/$(current_branch)..HEAD 2> /dev/null)" | grep '^commit' &> /dev/null); then
-    echo "$ZSH_THEME_GIT_PROMPT_AHEAD"
-  fi
-}
-
-# Formats prompt string for current git commit short SHA
-function git_prompt_short_sha() {
-  SHA=$(git rev-parse --short HEAD 2> /dev/null) && echo "$ZSH_THEME_GIT_PROMPT_SHA_BEFORE$SHA$ZSH_THEME_GIT_PROMPT_SHA_AFTER"
-}
-
-# Formats prompt string for current git commit long SHA
-function git_prompt_long_sha() {
-  SHA=$(git rev-parse HEAD 2> /dev/null) && echo "$ZSH_THEME_GIT_PROMPT_SHA_BEFORE$SHA$ZSH_THEME_GIT_PROMPT_SHA_AFTER"
-}
-
 # Get the status of the working tree
 git_prompt_status() {
   index=$(git status --porcelain 2> /dev/null)
