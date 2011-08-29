ed0c7416f4f3301773f6c88c4dac1dc578ff99c0
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-29 14:20:11 -0700

    Remove space from rbenv_prompt in prompt.zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,7 +6,7 @@ git_suffix=' '
 git_prompt_clean=":%{$fg[green]%}✔"
 git_prompt_dirty=":%{$fg[red]%}✘"
 
-rbenv_prompt=$'%{$fg[red]%}$(rbenv_prompt_info)%{$reset_color%} '
+rbenv_prompt=$'%{$fg[red]%}$(rbenv_prompt_info)%{$reset_color%}'
 git_prompt=$'%{$fg[magenta]%}$(git_prompt_info)%{$reset_color%}'
 dir_prompt=$'%{$fg[cyan]%}%~%{$reset_color%}'
 prompt_prompt=$'%{$fg[cyan]%}\n⍟%{$reset_color%} '
