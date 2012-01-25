0a05581c89b0790e35c496d852143418e8dfc565
Michael Hanson <michael.b.hanson@gmail.com>
2012-01-25 06:42:21 -0800

    Update prompt to show exit status of last command

--- zsh/prompt/prompt.zsh
+++ zsh/prompt/prompt.zsh
@@ -1,6 +1,8 @@
 autoload colors && colors
 setopt PROMPT_SUBST
 
+smiley="%(?,%{$fg[green]%}☺%{$reset_color%},%{$fg[red]%}☹%{$reset_color%})"
+
 git_prefix=''
 git_suffix=' '
 git_prompt_clean=":%{$fg[green]%}✔"
@@ -14,10 +16,9 @@ git_prompt_untracked="%{$fg_bold[cyan]%}?%{$reset_color%}"
 
 rbenv_prompt=$'%{$fg[red]%}$(rbenv_prompt_info)%{$reset_color%} '
 node_prompt=$'%{$fg[green]%}$(node_prompt_info)%{$reset_color%} '
-coffee_prompt=$'%{$fg[yellow]%}$(coffeescript_prompt_info)%{$reset_color%} '
 git_prompt=$'%{$fg[magenta]%}$(git_prompt_info)%{$reset_color%}'
 dir_prompt=$'%{$fg[cyan]%}%~%{$reset_color%}'
-prompt_prompt=$'%{$fg[white]%}\n›%{$reset_color%} '
+prompt_prompt=$'\n${smiley} '
 
-PROMPT="$rbenv_prompt$node_prompt$coffee_prompt$git_prompt$dir_prompt$prompt_prompt"
+PROMPT="$rbenv_prompt$node_prompt$git_prompt$dir_prompt$prompt_prompt"
 RPROMPT=$'%{$fg[black]%}%n@%m%{$reset_color%}'
