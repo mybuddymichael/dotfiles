dab83ad5d19fa069f7685fc1cce3c807daf9fe1e
Michael Hanson <michael.b.hanson@gmail.com>
2013-06-13 09:16:26 -0700

    Fish: Only parse git status if dirty

--- fish/functions/git-status-prompt.fish
+++ fish/functions/git-status-prompt.fish
@@ -14,34 +14,36 @@ function git-status-prompt
     set __status (echo -n -s "$symbol_clean")
   else
 
-  set -l untracked (for l in $index; echo $l; end | grep '^?? ')
-  if test "$untracked"
-    set __status (echo -n -s "$symbol_untracked$__status")
-  end
+    set -l untracked (for l in $index; echo $l; end | grep '^?? ')
+    if test "$untracked"
+      set __status (echo -n -s "$symbol_untracked$__status")
+    end
 
-  set -l added (for l in $index; echo $l; end | grep '^A  \|^M  \|^MM ')
-  if test "$added"
-    set __status (echo -n -s "$symbol_added$__status")
-  end
+    set -l modified (for l in $index; echo $l; end | grep '^ M \|^AM \|^ T \|^MM \|^RM ')
+    if test "$modified"
+      set __status (echo -n -s "$symbol_modified$__status")
+    end
 
-  set -l modified (for l in $index; echo $l; end | grep '^ M \|^AM \|^ T \|^MM \|^RM ')
-  if test "$modified"
-    set __status (echo -n -s "$symbol_modified$__status")
-  end
+    set -l added (for l in $index; echo $l; end | grep '^A  \|^M  \|^MM ')
+    if test "$added"
+      set __status (echo -n -s "$symbol_added$__status")
+    end
 
-  set -l renamed (for l in $index; echo $l; end | grep '^R  \|^RM ')
-  if test "$renamed"
-    set __status (echo -n -s "$symbol_renamed$__status")
-  end
+    set -l renamed (for l in $index; echo $l; end | grep '^R  \|^RM ')
+    if test "$renamed"
+      set __status (echo -n -s "$symbol_renamed$__status")
+    end
 
-  set -l deleted (for l in $index; echo $l; end | grep '^D  \|^AD ')
-  if test "$deleted"
-    set __status (echo -n -s "$symbol_deleted$__status")
-  end
+    set -l deleted (for l in $index; echo $l; end | grep '^D  \|^AD ')
+    if test "$deleted"
+      set __status (echo -n -s "$symbol_deleted$__status")
+    end
 
-  set -l unmerged (for l in $index; echo $l; end | grep '^UU ')
-  if test "$unmerged"
-    set __status (echo -n -s "$symbol_unmerged$__status")
+    set -l unmerged (for l in $index; echo $l; end | grep '^UU ')
+    if test "$unmerged"
+      set __status (echo -n -s "$symbol_unmerged$__status")
+    end
+
   end
 
   echo -s -n "$__status"
