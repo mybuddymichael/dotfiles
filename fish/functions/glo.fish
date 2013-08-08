3963ef3fd3acc58d61ee637f0da405af631deb9c
Michael Hanson <michael.b.hanson@gmail.com>
2013-08-07 21:51:44 -0700

    Fish: Add $argv to glo

--- fish/functions/glo.fish
+++ fish/functions/glo.fish
@@ -1,4 +1,4 @@
 function glo
 	
-  git log --graph --all --pretty=format:'%C(yellow)%h%Creset%C(red)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+  git log --graph --all --pretty=format:'%C(yellow)%h%Creset%C(red)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit $argv
 end
