692fab431e383d453673c24c40a2c19d20c6ea1d
Michael Hanson <michael.b.hanson@gmail.com>
2013-09-30 12:34:52 -0700

    Fish: update rd with a new IP address

--- fish/functions/rd.fish
+++ fish/functions/rd.fish
@@ -1,3 +1,3 @@
 function rd --description 'Connect to the work Window VM.'
-	rdesktop -d RAMSGATE -u mhanson -g 1300x700 -x 0x80 10.1.30.76 &
+	rdesktop -d RAMSGATE -u mhanson -g 1300x700 -x 0x80 10.1.30.84 &
 end
