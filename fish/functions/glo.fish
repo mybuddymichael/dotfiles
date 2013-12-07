a4f46ca4bd3c3228b149aa81e5632b32cce31c58
Michael Hanson <michael.b.hanson@gmail.com>
2013-12-07 14:52:36 -0800

    Fish: Don't have glo use --all

--- fish/functions/glo.fish
+++ fish/functions/glo.fish
@@ -1,4 +1,4 @@
 function glo
 	
-  git log --graph --all --pretty=format:'%C(yellow)%h%Creset%C(red)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit $argv
+  git log --graph --pretty=format:'%C(yellow)%h%Creset%C(red)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit $argv
 end
