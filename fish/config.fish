4df0acdedbf1dcded7063f0ed8351fc9c47b5dca
Michael Hanson <michael.b.hanson@gmail.com>
2013-12-17 09:04:42 -0800

    Fish: Adjust formatting

--- fish/config.fish
+++ fish/config.fish
@@ -137,20 +137,20 @@
 
 # Useful functions.
 
-function t; tree $argv; end
-function tmux-start -d "Starts a new tmux session with three vertical panes, or it attaches a session if one named "main" already exists."
-  tmux has-session -t main > /dev/null ^&1
+  function t; tree $argv; end
+  function tmux-start -d "Starts a new tmux session with three vertical panes, or it attaches a session if one named "main" already exists."
+    tmux has-session -t main > /dev/null ^&1
 
-  if test $status -eq 0
+    if test $status -eq 0
+      tmux attach -t main
+      return 0
+    end
+
+    tmux new-session -d -s main
+    tmux new-window -t main:2
+    tmux new-window -t main:3
+    tmux join-pane -s main:2 -t main:1
+    tmux join-pane -s main:3 -t main:1
+    tmux select-layout -t main even-horizontal
     tmux attach -t main
-    return 0
   end
-
-  tmux new-session -d -s main
-  tmux new-window -t main:2
-  tmux new-window -t main:3
-  tmux join-pane -s main:2 -t main:1
-  tmux join-pane -s main:3 -t main:1
-  tmux select-layout -t main even-horizontal
-  tmux attach -t main
-end
