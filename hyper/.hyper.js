70dd658b1ceec8e9aac22fac04b0895d6b0feb90
Michael Hanson <michael.b.hanson@gmail.com>
2018-05-08 10:15:51 -0700

    Hyper: Change my font

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -4,21 +4,22 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily:
+      '"IBM Plex Mono", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color (hex)
-    cursorColor: 'rgba(42, 161, 152, 0.6)',
+    cursorColor: "rgba(42, 161, 152, 0.6)",
 
-    cursorShape: 'BLOCK',
+    cursorShape: "BLOCK",
 
     // color of the text
-    foregroundColor: '#073642',
+    foregroundColor: "#073642",
 
     // terminal background color
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
 
     // border color (window, tabs)
-    borderColor: '#fff',
+    borderColor: "#fff",
 
     // custom css to embed in the main window
     css: `
@@ -28,30 +29,30 @@ module.exports = {
     `,
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    termCSS: '',
+    termCSS: "",
 
     // custom padding
-    padding: '12px 14px',
+    padding: "12px 14px",
 
     // some color overrides. see http://bit.ly/29k1iU2 for
     // the full list
     colors: [
-      '#073642',
-      '#dc322f',
-      '#859900',
-      '#b58900',
-      '#268bd2',
-      '#d33682',
-      '#2aa198',
-      '#eee8d5',
-      '#002b36',
-      '#cb4b16',
-      '#586e75',
-      '#657b83',
-      '#839496',
-      '#6c71c4',
-      '#93a1a1',
-      '#fdf6e3'
+      "#073642",
+      "#dc322f",
+      "#859900",
+      "#b58900",
+      "#268bd2",
+      "#d33682",
+      "#2aa198",
+      "#eee8d5",
+      "#002b36",
+      "#cb4b16",
+      "#586e75",
+      "#657b83",
+      "#839496",
+      "#6c71c4",
+      "#93a1a1",
+      "#fdf6e3"
     ]
   },
 
@@ -61,8 +62,7 @@ module.exports = {
   //   `hypersolar`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-  ],
+  plugins: [],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
