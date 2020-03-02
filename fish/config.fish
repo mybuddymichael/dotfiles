befec9da90d7c20a706d69683130875bc4ed8dc2
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2020-03-02 08:38:54 -0800

    Fish: Comment out tmux-start
    It was erroring out with the new version of Fish and I don't care to fix it at
    the moment.

--- fish/config.fish
+++ fish/config.fish
@@ -189,22 +189,22 @@ end
 function serve-this
     python -m SimpleHTTPServer 9000
 end
-function tmux-start -d "Starts a new tmux session with three vertical panes, or it attaches a session if one named "main" already exists."
-    tmux has-session -t main >/dev/null ^&1
+# function tmux-start -d "Starts a new tmux session with three vertical panes, or it attaches a session if one named "main" already exists."
+#     tmux has-session -t main >/dev/null ^&1
 
-    if test $status -eq 0
-        tmux attach -t main
-        return 0
-    end
+#     if test $status -eq 0
+#         tmux attach -t main
+#         return 0
+#     end
 
-    tmux new-session -d -s main
-    tmux new-window -t main:2
-    tmux new-window -t main:3
-    tmux join-pane -s main:2 -t main:1
-    tmux join-pane -s main:3 -t main:1
-    tmux select-layout -t main even-horizontal
-    tmux attach -t main
-end
+#     tmux new-session -d -s main
+#     tmux new-window -t main:2
+#     tmux new-window -t main:3
+#     tmux join-pane -s main:2 -t main:1
+#     tmux join-pane -s main:3 -t main:1
+#     tmux select-layout -t main even-horizontal
+#     tmux attach -t main
+# end
 function gifit -d "Creates a gif of the provided screen recording."
     set -l temp_dir (mktemp -d ./frames.XXXX)
     set -l current_dir (pwd)
