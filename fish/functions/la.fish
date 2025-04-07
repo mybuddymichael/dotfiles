c5edb7a80243feaf4f4c764bef378fe8180a5e67
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-04-07 09:28:04 -0700

    Fish: Adjust la function to take args

--- fish/functions/la.fish
+++ fish/functions/la.fish
@@ -1,5 +1,3 @@
 function la
-  set level
-  set -q argv[1]; or set level 2
-  eza --all --long --ignore-glob=".git" --octal-permissions --no-permissions --no-user --tree --icons --level=$level
+  eza --all --long --ignore-glob=".git" --octal-permissions --no-permissions --no-user --tree --icons --level=1 $argv
 end
