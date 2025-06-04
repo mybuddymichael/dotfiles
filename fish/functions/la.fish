256776170b9f2a6759ecf4585aaeddf06beab4ac
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-04 07:49:37 -0700

    Fish: Update la function to show .git

--- fish/functions/la.fish
+++ fish/functions/la.fish
@@ -1,3 +1,3 @@
 function la
-  eza --all --long --ignore-glob=".git" --octal-permissions --no-permissions --no-user --tree --icons --level=1 $argv
+  eza --all --long --octal-permissions --no-permissions --no-user --tree --icons --level=1 $argv
 end
