b448055b649e6831a3093e890495abf8bac3c67b
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-20 09:02:15 -0700

    Nvim: Fix conflicting themes

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -35,7 +35,7 @@ require('lazy').setup({
 
   require 'kickstart.plugins.blink-cmp',
 
-  require 'kickstart.plugins.tokyonight',
+  -- require 'kickstart.plugins.tokyonight',
 
   require 'kickstart.plugins.todo-comments',
 
