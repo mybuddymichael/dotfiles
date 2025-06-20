2db07274385dc48abc5905f2771bf22e789190e8
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-20 09:02:15 -0700

    Nvim: Add more autoformatting

--- nvim/lua/kickstart/plugins/conform.lua
+++ nvim/lua/kickstart/plugins/conform.lua
@@ -31,11 +31,13 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        go = { 'gofmt' },
+        javascript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+        jsonc = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
