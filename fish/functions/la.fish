19f6c3fa836a166221a56670e0bb24264829d07d
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-20 09:02:15 -0700

    Fish: Fix the la command

--- fish/functions/la.fish
+++ fish/functions/la.fish
@@ -1,3 +1,7 @@
 function la
-  eza --all --long --octal-permissions --no-permissions --no-user --tree --icons --group-directories-first --level=0 $argv
+  if test (count $argv) -eq 0
+    eza --all --long --octal-permissions --no-permissions --no-user --tree --icons --group-directories-first --level=0
+  else
+    eza --all --long --octal-permissions --no-permissions --no-user --tree --icons --group-directories-first --level=1 $argv
+  end
 end
