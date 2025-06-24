cea235c99061a154cb278671342d22669712d6f0
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-24 07:54:58 -0700

    Nvim: Clean up config formatting

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -24,24 +24,15 @@ require('lazy').setup({
   -- include a plugin definition from file lua/path/name.lua
 
   require 'kickstart.plugins.gitsigns',
-
   require 'kickstart.plugins.which-key',
-
   require 'kickstart.plugins.telescope',
-
   require 'kickstart.plugins.lspconfig',
-
   require 'kickstart.plugins.conform',
-
   require 'kickstart.plugins.blink-cmp',
-
-  -- require 'kickstart.plugins.tokyonight',
-
   require 'kickstart.plugins.todo-comments',
-
   require 'kickstart.plugins.mini',
-
   require 'kickstart.plugins.treesitter',
+  -- require 'kickstart.plugins.tokyonight',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
