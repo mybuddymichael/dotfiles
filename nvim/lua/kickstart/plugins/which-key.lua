4521cce205ed8bd5fe106e822eba7ade6846a1a4
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-23 23:25:45 -0700

    Nvim: Use different format for which-key

--- nvim/lua/kickstart/plugins/which-key.lua
+++ nvim/lua/kickstart/plugins/which-key.lua
@@ -17,6 +17,7 @@ return {
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
+      preset = 'helix',
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.o.timeoutlen
       delay = 0,
