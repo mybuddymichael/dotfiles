336f2bd488c424e628b95e7470eb84c2e9849090
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-23 21:57:43 -0700

    Nvim: Add TypeScript support to LSP and Treesitter

--- nvim/lua/kickstart/plugins/treesitter.lua
+++ nvim/lua/kickstart/plugins/treesitter.lua
@@ -5,7 +5,23 @@ return {
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'go', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'go',
+        'html',
+        'javascript',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
