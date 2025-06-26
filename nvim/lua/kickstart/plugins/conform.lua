e3bc30b1e167634ee3a5a910854532f35ce0a50f
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-26 14:59:14 -0700

    Nvim: Use prettier to format if it's a dependency

--- nvim/lua/kickstart/plugins/conform.lua
+++ nvim/lua/kickstart/plugins/conform.lua
@@ -1,3 +1,36 @@
+local function has_prettier_dependency()
+  local package_json = vim.fn.findfile('package.json', '.;')
+  if package_json == '' then
+    return false
+  end
+
+  local file = io.open(package_json, 'r')
+  if not file then
+    return false
+  end
+
+  local content = file:read '*all'
+  file:close()
+
+  local ok, json = pcall(vim.fn.json_decode, content)
+  if not ok then
+    return false
+  end
+
+  local deps = json.dependencies or {}
+  local devDeps = json.devDependencies or {}
+
+  return deps.prettier or devDeps.prettier or deps['@prettier/plugin-xml'] or devDeps['@prettier/plugin-xml']
+end
+
+local function get_js_formatters()
+  if has_prettier_dependency() then
+    return { 'prettierd', 'prettier', stop_after_first = true }
+  else
+    return { 'biome' }
+  end
+end
+
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
@@ -32,12 +65,12 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         go = { 'gofmt' },
-        javascript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-        javascriptreact = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-        typescript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-        typescriptreact = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-        json = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-        jsonc = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+        javascript = get_js_formatters,
+        javascriptreact = get_js_formatters,
+        typescript = get_js_formatters,
+        typescriptreact = get_js_formatters,
+        json = get_js_formatters,
+        jsonc = get_js_formatters,
       },
     },
   },
