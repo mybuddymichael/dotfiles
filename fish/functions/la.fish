2fd40689c9f4277f9dfdca34899c6c3c9033af18
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-26 21:57:14 -0700

    Fish: Format the la function

--- fish/functions/la.fish
+++ fish/functions/la.fish
@@ -1,7 +1,7 @@
 function la
-  if test (count $argv) -eq 0
-    eza --all --long --octal-permissions --no-permissions --no-user --tree --icons --group-directories-first --level=0
-  else
-    eza --all --long --octal-permissions --no-permissions --no-user --tree --icons --group-directories-first --level=1 $argv
-  end
+    if test (count $argv) -eq 0
+        eza --all --long --octal-permissions --no-permissions --no-user --tree --icons --group-directories-first --level=0
+    else
+        eza --all --long --octal-permissions --no-permissions --no-user --tree --icons --group-directories-first --level=1 $argv
+    end
 end
