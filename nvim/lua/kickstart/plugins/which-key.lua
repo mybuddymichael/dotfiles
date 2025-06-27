5fa409fa48bd65fe0f7a1b9148cfe9d83d35eb5c
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-26 21:57:14 -0700

    Nvim: Add notify to which-key

--- nvim/lua/kickstart/plugins/which-key.lua
+++ nvim/lua/kickstart/plugins/which-key.lua
@@ -21,6 +21,7 @@ return {
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.o.timeoutlen
       delay = 0,
+      notify = true,
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
