d8f6c2c05c6873a59427f3731a61ff80fed339f6
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-26 21:57:14 -0700

    Nvim: Init.lua formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,30 +53,33 @@ Kickstart Guide:
 
     (If you already know the Neovim basics, you can skip this step.)
 
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
+  Once you've completed that, you can continue working through **AND READING**
+  the rest of the kickstart init.lua.
 
   Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
+    This will open up a help window with some basic information about reading,
+    navigating and searching the builtin help documentation.
 
     This should be the first place you go to look when you're stuck or confused
     with something. It's one of my favorite Neovim features.
 
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
+    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp
+    documentation, which is very useful when you're not exactly sure of what
+    you're looking for.
 
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
+  I have left several `:help X` comments throughout the init.lua These are
+  hints about where to find more information about the relevant settings,
+  plugins or Neovim features used in Kickstart.
 
    NOTE: Look for lines like this
 
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
+    Throughout the file. These are for you, the reader, to help you understand
+    what is happening. Feel free to delete them once you know what you're
+    doing, but they should serve as a guide for when you are first encountering
+    a few different constructs in your Neovim config.
 
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
+If you experience any errors while trying to install kickstart, run
+`:checkhealth` for more info.
 
 I hope you enjoy your Neovim journey,
 - TJ
