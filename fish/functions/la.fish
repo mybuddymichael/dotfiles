1a239182f2dff4ff93a372c3e590e6857b8a0e2e
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-26 21:57:14 -0700

    Fish: Update eza

--- fish/functions/la.fish
+++ fish/functions/la.fish
@@ -1,7 +1,3 @@
 function la
-    if test (count $argv) -eq 0
-        eza --all --long --octal-permissions --no-permissions --no-user --tree --icons --group-directories-first --level=0
-    else
-        eza --all --long --octal-permissions --no-permissions --no-user --tree --icons --group-directories-first --level=1 $argv
-    end
+    eza --all --long --octal-permissions --no-permissions --no-user --tree --icons --group-directories-first --level=1 $argv
 end
