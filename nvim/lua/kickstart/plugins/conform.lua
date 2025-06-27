ebd04f4baadcf3cc357746f68a1f34d8240673b8
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-26 21:57:14 -0700

    Nvim: Fix conform

--- nvim/lua/kickstart/plugins/conform.lua
+++ nvim/lua/kickstart/plugins/conform.lua
@@ -31,47 +31,45 @@ local function get_js_formatters()
   end
 end
 
-return {
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
+return { -- Autoformat
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
       end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        go = { 'gofmt' },
-        javascript = get_js_formatters,
-        javascriptreact = get_js_formatters,
-        typescript = get_js_formatters,
-        typescriptreact = get_js_formatters,
-        json = get_js_formatters,
-        jsonc = get_js_formatters,
-      },
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
+      end
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      go = { 'gofumpt' },
+      javascript = get_js_formatters,
+      javascriptreact = get_js_formatters,
+      typescript = get_js_formatters,
+      typescriptreact = get_js_formatters,
+      json = get_js_formatters,
+      jsonc = get_js_formatters,
     },
   },
 }
