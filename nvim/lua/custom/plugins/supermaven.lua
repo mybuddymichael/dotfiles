308a0f83a6205e2f42a4ec736b8365303292e2a2
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-06-26 21:57:14 -0700

    Nvim: Adjust supermaven keybindings

--- nvim/lua/custom/plugins/supermaven.lua
+++ nvim/lua/custom/plugins/supermaven.lua
@@ -1,6 +1,12 @@
 return {
   'supermaven-inc/supermaven-nvim',
   config = function()
-    require('supermaven-nvim').setup {}
+    require('supermaven-nvim').setup {
+      keymaps = {
+        accept_suggestion = '<C-;>',
+        clear_suggestion = '<C-]>',
+        accept_word = '<C-l>',
+      },
+    }
   end,
 }
