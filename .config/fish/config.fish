4acae0a9eb99cebe89f509be97bdafe164914bd2
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-11-25 11:54:37 -0800

    Fish: Re-add color variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,26 +30,26 @@ abbr -a me mise exec --
 
 # Colors.
 
-# set fish_color_normal normal
-# set fish_color_command --bold
-# set fish_color_quote brown
-# set fish_color_redirection normal
-# set fish_color_end normal
-# set fish_color_error red --bold
-# set fish_color_param blue
-# set fish_color_comment red
-# set fish_color_match cyan
-# set fish_color_search_match --background=white
-# set fish_color_operator cyan
-# set fish_color_escape cyan
-# set fish_color_cwd cyan
-# set fish_color_autosuggestion 555 yellow
-# set fish_color_user -o green
-# set fish_color_host -o green
-#
-# set fish_pager_color_prefix cyan
-# set fish_pager_color_completion normal
-# set fish_pager_color_description brblue
+set -g fish_color_normal normal
+set -g fish_color_command --bold
+set -g fish_color_quote brown
+set -g fish_color_redirection normal
+set -g fish_color_end normal
+set -g fish_color_error red --bold
+set -g fish_color_param blue
+set -g fish_color_comment red
+set -g fish_color_match cyan
+set -g fish_color_search_match --background=white
+set -g fish_color_operator cyan
+set -g fish_color_escape cyan
+set -g fish_color_cwd cyan
+set -g fish_color_autosuggestion 555 yellow
+set -g fish_color_user -o green
+set -g fish_color_host -o green
+
+set -g fish_pager_color_prefix cyan
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description brblue
 
 # Set up utilities.
 atuin init fish | source
