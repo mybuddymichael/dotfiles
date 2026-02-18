9b685452f23191e62c17fa555d2254836fe2a749
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2026-02-18 11:33:00 -0800

    Sketchybar: Adjust date format

--- .config/sketchybar/plugins/clock.sh
+++ .config/sketchybar/plugins/clock.sh
@@ -2,4 +2,4 @@
 
 source "$CONFIG_DIR/colors.sh"
 
-sketchybar --set "$NAME" label="$(date '+%d %b %H.%M.%S')" icon.color="$CLOCK_COLOR" label.color="$CLOCK_COLOR"
+sketchybar --set "$NAME" label="$(date '+%a %d %b %H:%M:%S')" icon.color="$CLOCK_COLOR" label.color="$CLOCK_COLOR"
