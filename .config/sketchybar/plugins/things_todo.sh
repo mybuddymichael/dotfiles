4e8397d617a733a9c97dc4fb41d4c827fa724e04
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2026-02-18 11:49:29 -0800

    Sketchybar: Use sift to get top priority

--- .config/sketchybar/plugins/things_todo.sh
+++ .config/sketchybar/plugins/things_todo.sh
@@ -2,47 +2,26 @@
 
 source "$CONFIG_DIR/colors.sh"
 
-# Get the next todo from Things using AppleScript
-TODO=$(osascript -e '
-tell application "Things3"
-  set todayTodos to to dos of list "Today"
-  if (count of todayTodos) > 0 then
-    set nextTodo to item 1 of todayTodos
-    return name of nextTodo
-  else
-    return "No todos"
-  end if
-end tell
-' 2>/dev/null)
+# Get the highest-priority task from sift
+TODO="$(sift --top 2>/dev/null)"
+
+if [ -z "$TODO" ]; then
+  TODO="(No prioritized task)"
+fi
 
 # Truncate if too long
-if [ ${#TODO} -gt 30 ]; then
-  TODO="${TODO:0:27}..."
+if [ ${#TODO} -gt 40 ]; then
+  TODO="${TODO:0:37}..."
 fi
 
-# Show icon only when no todos, show with label when there are todos
-if [ "$TODO" = "No todos" ] || [ "$TODO" = "" ]; then
-  # Icon only - centered padding accounting for internal icon-label spacing
-  # Shift slightly right to compensate for internal spacing
-  sketchybar --set "$NAME" \
-    drawing=on \
-    icon=":things:" \
-    icon.color="$THINGS_TODO_ICON_COLOR" \
-    icon.padding_left=10 \
-    icon.padding_right=7 \
-    label="" \
-    label.padding_right=0 \
-    icon.font="sketchybar-app-font:Regular:14.0"
-else
-  # Icon with label - normal padding
-  sketchybar --set "$NAME" \
-    drawing=on \
-    icon=":things:" \
-    icon.color="$THINGS_TODO_ICON_COLOR" \
-    icon.padding_left=10 \
-    icon.padding_right=10 \
-    label="$TODO" \
-    label.color="$THINGS_TODO_LABEL_COLOR" \
-    label.padding_right=10 \
-    icon.font="sketchybar-app-font:Regular:14.0"
-fi
+# Icon with label
+sketchybar --set "$NAME" \
+  drawing=on \
+  icon=":things:" \
+  icon.color="$THINGS_TODO_ICON_COLOR" \
+  icon.padding_left=10 \
+  icon.padding_right=10 \
+  label="$TODO" \
+  label.color="$THINGS_TODO_LABEL_COLOR" \
+  label.padding_right=10 \
+  icon.font="sketchybar-app-font:Regular:14.0"
