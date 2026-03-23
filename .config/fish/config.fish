225e3aafd267b49f77b063050bec2e69b96f4fe6
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2026-03-23 12:17:48 -0700

    Fish: Clean pnpm env formatting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -158,8 +158,8 @@ zoxide init fish | source
 starship init fish | source
 
 # pnpm
-set -gx PNPM_HOME "/Users/michael/Library/pnpm"
+set -gx PNPM_HOME /Users/michael/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
