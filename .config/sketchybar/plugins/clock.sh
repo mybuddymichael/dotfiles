1149c5028a5c58bc75b49a5543ef6681b0083178
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2026-06-16 11:41:16 -0700

    Sketchybar: Remove obsolete config

--- .config/sketchybar/plugins/clock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-source "$CONFIG_DIR/colors.sh"
-
-sketchybar --set "$NAME" \
-  icon="􀐫" \
-  label="$(date '+%a %d %b %H:%M:%S')" \
-  icon.color="$CLOCK_COLOR" \
-  label.color="$CLOCK_COLOR"
